05a834632f26cc90acfbfb73a3fb79b2b79449ff
Gabriel Brinck da Silva <s33233@pjwstk.edu.pl>
2026-04-27 12:10:38 +0200

    Done some AppointmentService implementation

--- Tutorial7/Services/AppointmentService.cs
+++ Tutorial7/Services/AppointmentService.cs
@@ -1,4 +1,7 @@
-﻿namespace Tutorial7.Services;
+﻿using Microsoft.Data.SqlClient;
+using Tutorial7.DTOs;
+
+namespace Tutorial7.Services;
 
 public class AppointmentService : IAppointmentService
 {
@@ -7,5 +10,32 @@ public class AppointmentService : IAppointmentService
     public AppointmentService(IConfiguration configuration)
     {
         _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public async Task<IEnumerable<AppointmentListDto>> GetAllAppointmentsAsync() 
+    {
+        var query = "SELECT a.IdAppointment, a.AppointmentDate, a.Status, a.Reason, p.FirstName + N' ' + p.LastName AS PatientFullName, p.Email AS PatientEmail" +
+            "FROM dbo.Appointments a" +
+            "JOIN dbo.Patients p ON p.IdPatient = a.IdPatient" +
+            "WHERE (@Status IS NULL OR a.Status = @Status)" +
+            "  AND (@PatientLastName IS NULL OR p.LastName = @PatientLastName)" +
+            "ORDER BY a.AppointmentDate;";
+
+        await using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = query;
+        command.Parameters.AddWithValue("@Status", Status);
+        command.Parameters.AddWithValue("@PatientLastName", PatientLastName);
+
+        var reader = await command.ExecuteReaderAsync();
+        var appointments = new List<AppointmentListDto>();
+        while (await reader.ReadAsync())
+        {
+            var appointment = new AppointmentListDto();
+            appointments.Add(appointment);
+        }
     }
 }
--- Tutorial7/Services/IAppointmentService.cs
+++ Tutorial7/Services/IAppointmentService.cs
@@ -1,6 +1,8 @@
-﻿namespace Tutorial7.Services;
+﻿using Tutorial7.DTOs;
+
+namespace Tutorial7.Services;
 
 public interface IAppointmentService
 {
-    
+    Task<IEnumerable<AppointmentListDto>> GetAllAppointmentsAsync();
 }
